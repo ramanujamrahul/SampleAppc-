acbaa9fa6fe61485ac779155dff83cb3719c89e1
rahul ramanujam <ramanujamrahul@gmail.com>
2020-04-27 23:31:06 -0400

    html 5 basics

--- SampleApp/Startup.cs
+++ SampleApp/Startup.cs
@@ -21,6 +21,8 @@ namespace SampleApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
            
         }
     }
