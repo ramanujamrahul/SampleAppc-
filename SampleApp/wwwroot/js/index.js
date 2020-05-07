f316308b8b95ae2e884acc0a62f70bfebf62d5f4
rahul ramanujam <ramanujamrahul@gmail.com>
2020-05-06 21:13:44 -0400

    jquery

--- SampleApp/wwwroot/js/index.js
+++ SampleApp/wwwroot/js/index.js
@@ -1,11 +1,18 @@
-﻿console.log("wassup");
-var theFrom = $("#theForm");
-theFrom.hide();
-var button = $("#buyButton");
-button.on("click", function () {
-    console.log("Buying Item");
-});
-var productInfo = $(".product-props li");
-productInfo.on("click", function () {
-    console.log("you clicked on " + $(this).text())
+﻿$(document).ready(function () {
+    console.log("wassup");
+    var theFrom = $("#theForm");
+    theFrom.hide();
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("you clicked on " + $(this).text())
+    });
+    var loginToggle = $("#loginToggle");
+    var popupForm = $(".popup-form");
+    loginToggle.on("click", function () {
+        popupForm.toggle(1000);
+    });
 });
